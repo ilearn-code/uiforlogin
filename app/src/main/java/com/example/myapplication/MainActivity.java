ebad3ca692ad7b6537f94ba880d4981816e528fd
ilearn-code <satyamyadavv931@gmail.com>
2023-03-06 18:09:07 +0530

    dependencies and intewrnet permission

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.myapplication;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText Email, Password;
+    Button LoginButton;
+    RequestQueue requestQueue;
+    String EmailHolder, PasswordHolder;
+    ProgressDialog progressDialog;
+    String HttpUrl = "https://pg.imbuetech.in/api/login";
+    Boolean CheckEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Email = (EditText) findViewById(R.id.loginusername);
+        Password = (EditText) findViewById(R.id.loginPassword);
+        LoginButton = (Button) findViewById(R.id.button);
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        progressDialog = new ProgressDialog(MainActivity.this);
+
+
     }
 }
